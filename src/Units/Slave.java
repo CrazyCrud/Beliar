19fe14c186f8d3159b0e97cc7bd097a64c2b088a
Constantin Lehenmeier <constantin.lehenmeier@stud.uni-regensburg.de>
2012-10-01 13:10:32 +0200

    Slave

---
+++ src/Units/Slave.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Units;
+
+import com.jme3.scene.Node;
+
+/**
+ *
+ * @author andministrator
+ */
+public class Slave {
+    private Node mySlave;
+    private WalkControl walkControl;
+}
