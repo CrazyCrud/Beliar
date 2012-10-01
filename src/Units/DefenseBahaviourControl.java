d094db4a9500857101456689242da25c48c8c980
Constantin Lehenmeier <constantin.lehenmeier@stud.uni-regensburg.de>
2012-10-01 12:54:03 +0200

    Units

---
+++ src/Units/DefenseBahaviourControl.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Units;
+
+import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.ViewPort;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.control.AbstractControl;
+import com.jme3.scene.control.Control;
+
+/**
+ *
+ * @author andministrator
+ */
+public class DefenseBahaviourControl extends AbstractControl{
+
+    @Override
+    protected void controlUpdate(float tpf) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected void controlRender(RenderManager rm, ViewPort vp) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Control cloneForSpatial(Spatial spatial) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
