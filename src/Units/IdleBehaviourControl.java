908a3bbb7238a6785e76858331dd39df23e44300
Constantin Lehenmeier <constantin.lehenmeier@stud.uni-regensburg.de>
2012-10-01 14:06:37 +0200

    Units

--- src/Units/IdleBehaviourControl.java
+++ src/Units/IdleBehaviourControl.java
@@ -4,10 +4,30 @@
  */
 package Units;
 
+import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.ViewPort;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.control.AbstractControl;
+import com.jme3.scene.control.Control;
+
 /**
  *
  * @author andministrator
  */
-public class IdleBehaviourControl {
+public class IdleBehaviourControl extends AbstractControl{
+
+    @Override
+    protected void controlUpdate(float tpf) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected void controlRender(RenderManager rm, ViewPort vp) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Control cloneForSpatial(Spatial spatial) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
     
 }
