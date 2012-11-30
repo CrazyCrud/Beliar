1412a4b5c3c902238697eae013e5ee4456dfeca9
Martin <martin.dechant@web.de>
2012-11-30 01:36:07 +0100

    EditBÄM

--- src/Map/MapHandler.java
+++ src/Map/MapHandler.java
@@ -177,7 +177,7 @@ public class MapHandler {
         
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
-                terrainMap[x + i][y + j] = ValuesTerrain.SOULABYSS;
+                terrainMap[x + i][y + j] = 1;
             }
         }
         
